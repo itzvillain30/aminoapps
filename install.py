7566c518b07d0353e4ee6a83cef74f087f20f130
itzvillain30 <villainstore1@gmail.com>
2025-01-17 19:05:35 +0530

    Update install.py

--- install.py
+++ install.py
@@ -15,10 +15,4 @@ from json import load
 from json import dump
 data=load(open(_A))
 print(Fore.GREEN+'INSTALLATION DONE OF ALL REQUIRED PYTHON PACKAGE')
-print(Fore.RED+"CHOOSE A OPTION \n1.YES, I HAVE A KEY \n2.NO, I DON'T HAVEA KEY")
-ch=int(input(Fore.GREEN+'ENTER A OPTION HERE : '))
-if ch==1:
-	key=input(Fore.GREEN+'ENTER KEY HERE : ');data['keys']=key
-	with open(_A,'w')as file:dump(data,file,indent=4)
-	os.system('cls'if os.name=='nt'else'clear');print(Fore.GREEN+'KEY IS ADDED SUCESSFULLY !!');print(Fore.CYAN+'YOUR SCRIPT IS READY TO USE NOW !!')
-else:print(Fore.CYAN+'YOUR SCRIPT IS READY TO USE NOW BUT YOU NEED KEY FOR THE SCRIPT FROM OWNER')
+print(Fore.CYAN+'YOUR SCRIPT IS READY TO USE NOW BUT YOU NEED KEY FOR THE SCRIPT FROM OWNER')
